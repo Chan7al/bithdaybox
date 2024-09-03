b569fc9303e0fc6b73ae2b53a5c2bddf05f7a9c6
Chan7al <98766869+Chan7al@users.noreply.github.com>
2024-09-03 04:39:53 +0100

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Mar 01, 2022',
+  birthdate: 'Mar 01, 2024',
   name: '💜LUFFY💜'
 };
 
